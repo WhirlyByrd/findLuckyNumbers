6ab5e57973a8a195fcd4f33c9ade6e3a2ffb9289
WhirlyByrd <kara.r.gardner@gmail.com>
2022-08-23 10:00:50 -0500

    first commit

---
+++ findLuckyNumbers.js
@@ -0,0 +1,27 @@
+/* 
+Given a number, n, return an array containing n unique random numbers between 1-10, inclusive. 
+(That is, do not repeat any numbers in the returned list.)
+
+You can trust that this function will never be called with n < 0 or n > 10.
+
+For example:
+
+luckyNumbers(2)
+ returns (3, 7)
+luckyNumbers(6)
+ returns (1, 7, 9, 6, 5, 2) 
+ 
+ */
+
+ // Math.floor - rounds off numbers down to smaller number
+ // Math.random - generates random numbers
+
+ // Math.floor(Math.random) - combines the two functions to generate random whole numbers
+
+ // Math.floor(Math.random) * 11 - returns a random integer from 0 - 10
+
+ for(){
+
+ }
+
+ luckyNumbers()
